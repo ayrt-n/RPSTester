5c73c7753abf9cfe98ae2a44039d150599c8e334
ayrton <ayrton.parkinson1@gmail.com>
2024-07-13 10:33:24 -0400

    Initial Commit

---
+++ RockPaperScissorsTester/RockPaperScissorsTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsTesterApp.swift
+//  RockPaperScissorsTester
+//
+//  Created by Ayrton Parkinson on 2024/07/13.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
